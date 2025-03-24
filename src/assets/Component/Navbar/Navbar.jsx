80f1d36de300ac7826b27d706eb860e6117a5b36
Mmohima Modak <mohimamodak316@gmail.com>
2025-03-24 12:25:53 +0600

    Resolved merge conflicts in README.md

--- src/assets/Component/Navbar/Navbar.jsx
+++ src/assets/Component/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import menu from "../../../Gallery/menu.png"
-import Food from "../../../Gallery/Food11.png"
+import logo from "../../../Gallery/logo.png"
 import { Link } from "react-scroll";
 import "./Navbar.css"
 
@@ -13,12 +13,16 @@ export default function Navbar() {
 
   return (
     <div className="fixed w-full text-orange-500 font-medium top-0 z-50">
-      {/* Mobile Navbar */}
+
       <div className="md:hidden sm:block text-sm" onClick={toggleButton}>
         <span className="cursor-pointer p-5"><img src={menu} className="w-10" alt="menu" /></span>
       </div>
       <div className={`mobile-menu ${toggle ? "open" : ""}`}>
-        <div className="navbar-links">
+        <div className="flex justify-center items-center gap-2">
+          <img src={logo} className="w-14" />
+          <h1 className="logo text-4xl font-bold text-orange-600">foodie</h1>
+        </div>
+        <div className="navbar-links font-bold text-orange-600">
           <Link to="/" smooth={true} duration={1000} className="text-xl p-4 block" onClick={toggleButton}>
             Home
           </Link>
@@ -43,10 +47,14 @@ export default function Navbar() {
         </div>
       </div>
 
-      {/* Desktop Navbar */}
-      <div className="flex justify-center items-center ">
+
+      <div className="flex justify-between items-center ">
         <div className="hidden md:block bg-orange-100 w-full py-2">
-          <nav className="navbar flex justify-center items-center">
+          <nav className="navbar flex justify-between items-center">
+            <div className="flex justify-between items-center gap-2">
+              <img src={logo} className="w-14" />
+              <h1 className="logo text-4xl font-bold text-orange-600">foodie</h1>
+            </div>
             <div className="navbar-links lg:flex justify-center items-center">
               <Link to="/" smooth={true} duration={1000} className="text-xl mr-5 font-bold">
                 Home
